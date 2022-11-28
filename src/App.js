a17d767ae26fd6064903d30a1fcdf70714e99105
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-11-29 01:29:53 +0300

    Mapping array of monsters

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
 import { Component } from 'react';
-
 import './App.css';
-import logo from './logo.svg';
 
 
 class App extends Component {
     constructor() {
         super();
-        
+
         this.state = {
-            name: 'Dmitry',
-            company: 'Academy TOP'
+            monsters: [
+                {
+                    name: 'Frank',
+                    id: 1
+                },
+                {
+                    name: 'Mal',
+                    id: 2
+                },
+                {
+                    name: 'Jacky',
+                    id: 3
+                }
+            ]
         };
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img
-                        src={logo}
-                        className="App-logo"
-                        alt="logo"
-                    />
-                    <p>
-                        Hello, { this.state.name }!<br/>
-                        I'm working at { this.state.company }.
-                    </p>
-                    <button
-                        onClick={() => {
-                            // Установка состояния и синхронное чтение
-                            /* this.setState({ name: 'Superman' });
-                            console.log('this.state :>> ', this.state); */
-                            
-                            // Установка состояния и асинхронное чтение
-                            this.setState(
-                                () => ({ name: 'Superman' }),
-                                () => console.log('this.state :>> ', this.state)
-                            );
-                        }}
-                    >
-                        Change name
-                    </button>
-                </header>
+                { this.state.monsters.map(monster => {
+                    return (
+                        <div key={ monster.id }>
+                            <h2>{ monster.name }</h2>
+                        </div>
+                    );
+                }) }
             </div>
         );
     }

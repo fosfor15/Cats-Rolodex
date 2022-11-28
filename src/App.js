dbd286b4963e5d44ac85b8b7661201a0c04036a7
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-11-29 01:08:03 +0300

    Class component, working with state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
+import { Component } from 'react';
+
 import './App.css';
 import logo from './logo.svg';
 
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img
-                    src={logo}
-                    className="App-logo"
-                    alt="logo"
-                />
-                <p>
-                    Hello! My name is Dmitry!
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            name: 'Dmitry',
+            company: 'Academy TOP'
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img
+                        src={logo}
+                        className="App-logo"
+                        alt="logo"
+                    />
+                    <p>
+                        Hello, { this.state.name }!<br/>
+                        I'm working at { this.state.company }.
+                    </p>
+                    <button
+                        onClick={() => {
+                            // Установка состояния и синхронное чтение
+                            /* this.setState({ name: 'Superman' });
+                            console.log('this.state :>> ', this.state); */
+                            
+                            // Установка состояния и асинхронное чтение
+                            this.setState(
+                                () => ({ name: 'Superman' }),
+                                () => console.log('this.state :>> ', this.state)
+                            );
+                        }}
+                    >
+                        Change name
+                    </button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
